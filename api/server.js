393c31fd75c4a573a67bb61d8e813373e05ba055
Steve <steve@yarrumevets.com>
2026-08-25 19:18:55 -0700

    Added .env.example and installed dep and implemented dotenv in api server to use env vars for NODE_ENV and port.

--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
 import express from "express";
+import "dotenv/config";
+
+const PORT = process.env.PORT;
+const NODE_ENV = process.env.NODE_ENV;
 
 const app = express();
-const PORT = 3949;
+
 app.use(express.json());
 app.use(express.static("public"));
 
@@ -11,5 +15,5 @@ app.get("/health", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`API running on http://localhost:${PORT}`);
+  console.log(`API running on http://localhost:${PORT} (${NODE_ENV})`);
 });
